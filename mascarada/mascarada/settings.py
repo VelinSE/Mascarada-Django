ea531e623bcf3984207113299adaf4d1f20301fc
A. Staykov <a.staykov@student.fontys.nl>
2018-06-16 16:24:59 +0200

    pipefile resovled

--- mascarada/mascarada/settings.py
+++ mascarada/mascarada/settings.py
@@ -139,6 +139,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+ALLOWED_HOSTS = [
+    'quiet-lowlands-96728.herokuapp.com'
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
